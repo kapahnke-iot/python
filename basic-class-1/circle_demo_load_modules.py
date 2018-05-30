b459f9b1b3b39278d2afbb40a0308340733c87ac
Bernd Kapahnke <kapahnke@web.de>
2018-05-30 23:04:29 +0000

    python class basic 1

---
+++ basic-class-1/circle_demo_load_modules.py
@@ -0,0 +1,6 @@
+print(dir())
+from math import pi
+print(dir())
+radius = input ('What is the circle radius?')
+print ('The area of the circle is', float(radius) ** 2 * pi)
+print ('The circumerence of the circle is', 2 * float(radius) * pi)
